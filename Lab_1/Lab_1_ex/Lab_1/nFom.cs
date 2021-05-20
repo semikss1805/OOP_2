5a4ca17ae5a6ed4d0263f21a983f3b936387ac66
semikss1805 <semikss1805fw@gmail.com>
2021-05-21 00:21:08 +0300

    lab_1

---
+++ Lab_1/Lab_1_ex/Lab_1/nFom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_1
+{
+    public partial class nFom : Form2
+    {
+        public nFom()
+        {
+            InitializeComponent();
+            button1.Text = "Hello!!!";
+            button1.BackColor = Color.Brown;
+
+        }
+    }
+}
